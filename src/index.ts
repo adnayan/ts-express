05776d8e57efc2731948af94e5abd9d4e4e8309e
Nayan Adhikari <adnayan@hotmail.com>
2021-01-16 00:24:27 +0100

    formatting params

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from 'express';
 
 const app: Application = express();
-const PORT: number = 5000;
+const PORT = 5000;
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-    res.json("Hello from application.");
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+	res.json('Hello from application.');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}.`);
+	console.log(`Server is running at http://localhost:${PORT}.`);
 });
