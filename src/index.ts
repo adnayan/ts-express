edbb8e716772888bbeab75c8b6ff3ee0da6b3828
Nayan Adhikari <adnayan@hotmail.com>
2021-01-23 00:01:11 +0100

    emergency commit for development device change

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
+import 'reflect-metadata';
+import express, { Application, Request, Response, NextFunction } from 'express';
+import { createConnection } from 'typeorm';
 
-const app: Application = express();
-const PORT = 5000;
+const main = async () => {
+	const app: Application = express();
+	await createConnection();
+	const PORT = 5000;
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-	res.json('Hello from application.');
-});
+	app.get('/', (req: Request, res: Response, next: NextFunction) => {
+		res.json('Hello from application.');
+	});
 
-app.listen(PORT, () => {
-	console.log(`Server is running at http://localhost:${PORT}.`);
-});
+	app.listen(PORT, () => {
+		console.log(`Server is running at http://localhost:${PORT}.`);
+	});
+};
+
+main();
