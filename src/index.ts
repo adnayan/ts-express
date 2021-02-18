05a2947b5208cfa5a529e48c93a4e7b482f65cf6
Nayan Adhikari <adnayan@hotmail.com>
2021-02-18 19:32:23 +0100

    dotenv, config, db service, socket service added, docker removed

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,35 @@
 import 'reflect-metadata';
+import 'dotenv/config';
+import { Server } from 'http';
 import express, { Application, Request, Response, NextFunction } from 'express';
-import { createConnection } from 'typeorm';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+
+import config from './config/config';
+import { connectDB } from './service/db';
+import SocketService from './service/socket';
+
+const app: Application = express();
+const server = new Server(app);
+
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+
+app.set('socketService', new SocketService(server));
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+	res.status(200).json('Hello from application.');
+});
 
 const main = async () => {
-	const app: Application = express();
-	await createConnection();
-	const PORT = 5000;
-
-	app.get('/', (req: Request, res: Response, next: NextFunction) => {
-		res.json('Hello from application.');
-	});
-
-	app.listen(PORT, () => {
-		console.log(`Server is running at http://localhost:${PORT}.`);
+	app.listen(config.PORT, () => {
+		console.log(`Server is running at http://localhost:${config.PORT}.`);
 	});
 };
 
-main();
+(async () => {
+	await connectDB();
+	await main();
+})();
